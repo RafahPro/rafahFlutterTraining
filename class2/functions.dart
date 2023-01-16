75f6cac5d22e3bbc5d09315235cb94182fe32b1f
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-16 22:44:45 +0200

    function introduction

--- class2/functions.dart
+++ class2/functions.dart
@@ -1,7 +1,13 @@
 //Dart Functions Introduction
 
-void main()
-{
-
-
+void main() {
+  int y = add(1, 3);
+  print(y);
 }
+
+int add(int a, int b) {
+  return a + b;
+}
+
+//Dart Functions Parameters
+
