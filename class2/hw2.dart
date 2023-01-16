e9a9af536bbcf2509fef62126cfcd06575e7ad4d
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-16 22:49:16 +0200

    homework

---
+++ class2/hw2.dart
@@ -0,0 +1,8 @@
+//Answer the following questions in Dart
+
+//1. Print Even Numbers from 1 to 100
+
+//2. Design a calculator of addition, subtraction, multiplication, division
+
+//3. Given a list of numbers [10,20,50,20,90] and returns the sum of all the numbers
+
