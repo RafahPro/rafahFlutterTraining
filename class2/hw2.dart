d3d53fd0c19ab2e2134554637271e71d037ac39c
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-18 18:28:16 +0200

    add antoher 2 questions by Eng.Ziad

--- class2/hw2.dart
+++ class2/hw2.dart
@@ -6,3 +6,6 @@
 
 //3. Given a list of numbers [10,20,50,20,90] and returns the sum of all the numbers
 
+//4 Print Number 7 Multiplication Table
+
+//5 Make a function that return True or false if the number is divisible by 3
