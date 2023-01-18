d5db5d88d53cfa7d50ad00134950fd2399240ef1
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-01-18 23:40:11 +0200

    Practice 1

---
+++ class3/practice1.dart
@@ -0,0 +1,13 @@
+//1. Rewrite the following code using arrow syntax.
+double resize(double size) {
+  return size * 0.75;
+}
+
+//2. Write a function & program that returns the cube of a number.
+
+
+//3. Write a function & program that returns the number of vowels in a string. 
+
+  // - The vowels in English are **a, e, i, o, and u**.
+
+  
